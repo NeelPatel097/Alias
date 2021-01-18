740610a6d5973763d6317d8f30a9a3d75d65e06f
Neel Patel <np7.patel.np@gmail.com>
2021-01-18 12:16:38 +0530

    Second Commit

---
+++ src/routes/auth.ts
@@ -0,0 +1,7 @@
+import { Request } from "express";
+
+
+
+const register = (req: Request, res: Response) => {
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import "reflect-metadata"
+import {createConnection} from "typeorm"
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/', (, res) => res.send('Hello Neel'))
+
+app.listen(5000, async () => {
+    console.log('Server running at localhost:5000')
+
+    try {
+        await createConnection()
+        console.log('Database connected!')
+    } catch (error) {
+        console.log(error)
+    }
+})
 
